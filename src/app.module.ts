6b6c8bc4a5ca200abedbb304d755b510876ea410
ppkgtmm <pkgtm1998@gmail.com>
2021-05-05 18:14:24 +0700

    generate files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventService } from './event/event.service';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [],
+  imports: [EventModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EventService],
 })
 export class AppModule {}
