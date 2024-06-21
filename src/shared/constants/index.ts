c5552183131d52b2a264b6311b7d99e110ad36f8
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-21 17:45:32 +0700

    Update index.ts

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,4 +1,4 @@
-export const MINYEAR = new Date().getFullYear();
+export const MINYEAR = 2021;
 export const MAXYEAR = 9999;
 export const OFFSET = 'GMT+7:00';
 export const SECINDAY = 24 * 60 * 60 * 1000;
